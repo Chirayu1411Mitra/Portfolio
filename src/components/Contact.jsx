a1e186daf2faea77b979247504c4c8828a93d844
Chirayu1411Mitra <mitrachirayu@gmail.com>
2024-11-06 11:30:42 +0530

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.service_4113tzo,
+        import.meta.env.template_1mvqyrq,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Phoenix",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "Shitkara7410@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.MHy9DhDtqDlekz66U
       )
       .then(
         () => {
